c8ad5d941042a8d2858a4e5c172b5386744e8c07
Ricky Payares <rpayaresswe@gmail.com>
2026-03-27 15:03:10 -0400

    Setup
    First Push - Setup Targets.

---
+++ Sonex/Sonex Clip View/Sonex_Clip_ViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sonex_Clip_ViewApp.swift
+//  Sonex Clip View
+//
+//  Created by Ricardo Payares on 3/27/26.
+//
+
+import SwiftUI
+
+@main
+struct Sonex_Clip_ViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Sonex/Sonex/SonexApp.swift
@@ -0,0 +1,17 @@
+//
+//  SonexApp.swift
+//  Sonex
+//
+//  Created by Ricardo Payares on 3/27/26.
+//
+
+import SwiftUI
+
+@main
+struct SonexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
